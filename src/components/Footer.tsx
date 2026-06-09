3de393cd88edd957655a2149a10b4b80bd8b7c79
Abhijith B <babhijith58@gmail.com>
2026-06-09 13:38:34 +0530

    feat: add components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer>
+            <p>© 2026 Knockout Boxing Club</p>
+        </footer>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,6 @@
+export default function Navbar(){
+    return(
+        <nav>
+            <h2>Knockout Boxing Club</h2>
+        </nav>   )
+}
