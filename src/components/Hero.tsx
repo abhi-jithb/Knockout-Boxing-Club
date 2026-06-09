19aca1dc848cb5ef98cf6ab0c20b7f18aae8e581
Abhijith B <babhijith58@gmail.com>
2026-06-09 14:15:45 +0530

    feat: add Hero Component button and styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,19 @@
 export default function Hero(){
     return(
-        <section>
-            <h1>Train Like A Champion</h1>
-            <p>Strength. Discipline. Boxing.</p>
+        <section className="min-h-[80vh] flex flex-col justify-center items-center text-center px-6">
+
+            <h1 className="text-6xl font-bold mb-6">
+                Train Like A Champion
+            </h1>
+
+            <p className="text-xl max-w-2xl mb-8">
+                Professsional Boxing Training for Beginners, Amateurs And Future Champions.
+            </p>
+
+            <button className="bg-black text-white px-6 py-3 rounded-lg">
+                Join Today
+            </button>
+
         </section>
     )
 }
