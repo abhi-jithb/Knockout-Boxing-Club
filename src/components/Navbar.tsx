3b1409a1c0d5f2de1bf64ec8e402bef85369016d
Abhijith B <babhijith58@gmail.com>
2026-06-09 13:57:32 +0530

    style: add styles on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,15 @@
 export default function Navbar(){
     return(
-        <nav>
-            <h2>Knockout Boxing Club</h2>
-        </nav>   )
+        <nav className="flex justify-between items-center px-8 py-4 border-b">
+            <h1 className="text-2xl font-bold">Knockout Boxing Club</h1>
+
+            <ul className="flex gap-6">
+                <li>Home</li>
+                <li>Programs</li>
+                <li>Trainers</li>
+                <li>Contact</li>
+            </ul>
+
+        </nav>   
+    )
 }
